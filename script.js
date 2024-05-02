0369b045db5e9e9ae3cfce6354233fa08d53411d
isak123isak <isak123isak@hotmail.com>
2024-05-03 01:33:33 +0200

    Todo-List

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-function addTodoTask() {
-  event.preventDefault();
-  // Array without content
-  const todoTask = [];
-  // Get fetches retrives from HTML document
-  const taskInput = document.querySelector('#taskInput').value;
-  // Fills the array with content from taskInput
-  todoTask.push(taskInput);
-  // Checks if todoTask have content
-  console.log(todoTask);
+const input = document.querySelector("input");
+const btn = document.querySelector(".addTask > button");
+
+function addList(e){
+    const notCompleted = document.querySelector(".notCompleted");
+    
+    const Completed = document.querySelector(".Completed");
+
+    const newLi = document.createElement("li");
+    const checkBtn = document.createElement("button");
+    const delckBtn = document.createElement("button");
+
+    checkBtn.innerHTML = <i class="fa-solid fa-circle-check"></i>;
+    delckBtn.innerHTML = <i class="fa-regular fa-trash-can"></i>;
+
 }
-
-const taskButton = document.querySelector('#taskButton');
-console.log(taskButton);
-
-taskButton.addEventListener('click', addTodoTask);
-// Loops through array and for new data
-for (index = 0; index < TodoTasks.length; index = index + 1) {
-  // Get data from our loop using to run through whats inside array
-  const newTodoTask = todoTask[index];
-
-  //Create a new HTML-element "li"
-  const newListItem = document.createElement('li');
-  console.log(newListItem);
-
-  // Set that the element "li" have content from our looped through data
-  newListItem.textContent = newTodoTasks;
-}
-
